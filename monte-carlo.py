aeeeb98d5f0a0a86007b31e0abda48de974d8ff8
zettatron <seth@abacusinsights.com>
2021-01-18 07:20:16 -0500

    monte carlo simulation, bsm model

---
+++ monte-carlo.py
@@ -0,0 +1,19 @@
+import math
+import numpy as np
+
+S0 = 100.
+K = 105.
+T = 1.0
+r = 0.05
+sigma = 0.2
+
+I = 1000000
+
+np.random.seed(1000)
+
+z = np.random.standard_normal(I)
+ST = S0 * np.exp((r - sigma ** 2 / 2) * T + sigma * math.sqrt(T) * z)
+hT = np.maximum(ST - K, 0)
+C0 = math.exp(-r * T) * np.mean(hT)
+
+print('Value of European call option: {:5.3f}.'.format(C0))
